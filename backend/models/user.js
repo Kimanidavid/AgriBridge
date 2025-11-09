e4baf433182c5d22a505e765e8dfc5119c82b163
kimanidavid <kimanidavid341@gmail.com>
2025-11-09 03:26:00 +0300

    Initial commit

---
+++ backend/models/user.js
@@ -0,0 +1,38 @@
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
